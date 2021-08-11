3dd0af189604cbe9b69b28d77854464ac947c119
Izabela Żmujdzin <izabela.zmujdzin@gmail.com>
2021-08-11 11:04:54 +0200

    aboutMe mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,20 @@
 import '../scss/main.scss';
 
-// uncomment the lines below to enable PWA
-// import {registerSW} from './pwa.js';
-// registerSW();
 
-/* place your code below */
 
-console.log('HELLO 🚀')
+fetch('https://api.github.com/repos/izabelazmujdzin/business-card')
+.then(resp => resp.json())
+.then(resp => {
+    for (let repo of resp) {
+        const {name, description, homepage, html_url} = repo;
+        const repositoryList = document.querySelector('.list--js');
+        const myTemplate = `<li>
+            ${name} </li>; <li> ${description}</li>; <li> <a href="${homepage}" title="link do strony">see here<\a></li>
+            <li> <a href="${html_url}" title="link do repozytorium na githubie">code<\a></li>`;
+        repositoryList.innerHTML += myTemplate;
+    }
+})
+
+.catch(error => {
+    console.log('error');
+})
