2b21c12899912bf1e11f225fcda698b947e6ae72
Izabela Żmujdzin <izabela.zmujdzin@gmail.com>
2021-09-01 14:24:27 +0200

    animation, js repositories, links in new tab, smooth scroll

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,33 @@
 import '../scss/main.scss';
 
+console.log ("Hi, I'm Iza. I'm glad you're here! 😊" )
 
 
-fetch('https://api.github.com/repos/izabelazmujdzin/business-card')
-.then(resp => resp.json())
-.then(resp => {
-    for (let repo of resp) {
-        const {name, description, homepage, html_url} = repo;
-        const repositoryList = document.querySelector('.list--js');
-        const myTemplate = `<li>
-            ${name} </li>; <li> ${description}</li>; <li> <a href="${homepage}" title="link do strony">see here<\a></li>
-            <li> <a href="${html_url}" title="link do repozytorium na githubie">code<\a></li>`;
-        repositoryList.innerHTML += myTemplate;
-    }
+fetch("https://api.github.com/users/izabelazmujdzin/repos?sort=created&direction=asc")
+.then((res) => res.json())
+.then((res) => {
+   const container = document.querySelector('.projects-grid--js');
+   for (let repo of res) {
+     const {description, homepage,html_url,name} = repo;
+
+     const template =`<article class="project">
+     <div class=project__window>
+       <span class="project__circle"></span>
+       <span class="project__circle"></span>
+       <span class="project__circle"></span>
+     </div>
+     <img class="project__emoji" src="img/githubwhite.png" alt="">
+     <div class="project__content">
+     <h3 class="project__grid project__title"><span class="project__label">projects: </span><span class="project__title--color">${name}</span></h3>
+     <p class="project__grid"><span class="project__label">description: </span> <span>${description}</span></p>
+     <p class="project__grid"><span class="project__label">demo: </span> <span><a class="project__link" target="_blank"  rel= "noopener noreferrer" href="${homepage}" title="${name}-demo">&lt;see here&gt;</a></span></p>
+     <p class="project__grid"><span class="project__label">github: </span> <span>&lt;<a class="project__link" target="_blank"  rel= "noopener noreferrer" href="${html_url}" title="${name}-link">source_code</a>&gt;</span></p>
+   </div>
+   </article>`
+   if (description){
+   container.innerHTML += template;
+   }
+   }
 })
-
-.catch(error => {
-    console.log('error');
-})
+.catch((e) => 
+    console.log(e));
